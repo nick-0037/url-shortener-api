9bb97d1dd37c5eb990d6b968533e06c329f4ce96
nick-0073 <120076510+nick-0037@users.noreply.github.com>
2024-12-26 12:50:31 -0400

    chore: remove uniqueness constraint  from  the 'url' field

--- models/Url.js
+++ models/Url.js
@@ -1,7 +1,7 @@
 const { Schema, model} = require('mongoose');
 
 const urlSchema = new Schema({
-  url: { type: String, unique: true, required: true },
+  url: { type: String, required: true },
   shortCode: { type: String, unique: true, required: true },
   accessCount: { type: Number, default: 0 }
 }, { timestamps: true });
